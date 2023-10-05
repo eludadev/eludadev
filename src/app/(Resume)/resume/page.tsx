4f1d85321518e594f594f1e137451b8d48cab298
Younes Laaroussi <01ylaaroussi01@gmail.com>
2023-10-05 18:44:36 +0100

    Update portfolio to most recent experience.

--- src/app/(Resume)/resume/page.tsx
+++ src/app/(Resume)/resume/page.tsx
@@ -11,13 +11,13 @@ export default function Resume(): JSX.Element {
   return (
     <div className="bg-primary-50 flex flex-col lg:my-12 lg:max-w-5xl lg:mx-24 lg:pt-24 pb-12 lg:grid lg:grid-cols-[1fr,3fr]">
       <div className="flex items-center gap-3 ml-auto w-fit mr-12 lg:mr-24 my-16 lg:my-0 lg:col-start-2 lg:row-start-1 self-start z-10">
-        <a
+        {/* <a
           className="text-primary-600"
           href={i18n.t('links.linkedin.value')}
           aria-label={i18n.t('links.linkedin.title')}
         >
           <LinkedInLogo />
-        </a>
+        </a> */}
         <a
           href={i18n.t('resume.profile.email.link')}
           className="text-xs text-black/60 font-normal flex items-center gap-1 lg:hidden"
